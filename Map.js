5b04d85d6be0e992c42c5419e83e26d083c7e56a
HariRai123 <harirai4132@gmail.com>
2025-10-14 15:16:46 +0530

    Set and Map added

--- Map.js
+++ Map.js
@@ -1,1 +1,37 @@
-//Map is an key value pair element
+//Map is an container which stores key value pair as element
+// set is used to add Element;
+// get is used to retrieval of value by passing key as an argument
+// size
+// has() returns whether the Element is available or not
+// delete() deletes the value
+// values() returns the values 
+// keys()  returns the keys
+// entries() returns the entries
+let map=new Map();
+map.set("harirai@gmail.com","Harirai123");
+map.set("reethika@gmail.com","Reethika53");
+map.set("Tilak@gmail.com","Tilak123")
+map.set("Tilak@gmail.com","Tillu2")
+console.log(map)
+console.log(map.size)
+console.log(map.get("reethika@gmail.com"))
+console.log(map.has("harirai@gmail.com"))
+console.log(map.has("hari@gmail.com"))
+//console.log(map.delete("Tilak@gmail.com"));
+console.log(map)
+let key=map.keys()
+for(let i of key)
+{
+    console.log(i)
+}
+let val=map.values()
+for(let i of val)
+{
+    console.log(i)
+}
+let entries=map.entries();
+for(let i of entries)
+{
+    console.log(i)
+    console.log(`${i[0]}    ${i[1]}`)
+}
--- Set.js
+++ Set.js
@@ -4,5 +4,23 @@ s.add(100);
 s.add(200);
 s.add(50);
 s.add(100);
+s.add(300);
 console.log(s);
 console.log(s.size);
+//let arr=[...s];
+for(let i of s)
+{
+    console.log(i)
+}
+s.delete(100)
+console.log(s)
+console.log(s.has(100))
+s.clear()
+console.log(s)
+
+
+// const employee={
+//     id:53,
+//     name:"Reethu",
+//     location:"TEXAS "
+// }
